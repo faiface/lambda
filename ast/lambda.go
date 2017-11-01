c4bef6eb00ab03282fc7e02bd7cba387c1660636
faiface <faiface@ksp.sk>
2017-11-01 02:44:11 +0100

    add basic lambda calculus "runtime" and AST compiler

---
+++ ast/lambda.go
@@ -0,0 +1,169 @@
+package ast
+
+import (
+	"fmt"
+
+	"github.com/faiface/lambda/machine"
+)
+
+func CompileAll(globals map[string]Node) (map[string]machine.FreeExpr, error) {
+	var globalNames []string
+	for name := range globals {
+		globalNames = append(globalNames, name)
+	}
+
+	compiled := make([]machine.FreeExpr, len(globalNames))
+
+	compiledPtrs := make(map[string]*machine.FreeExpr)
+	for i, name := range globalNames {
+		compiledPtrs[name] = &compiled[i]
+	}
+
+	for i, name := range globalNames {
+		var err error
+		compiled[i], err = globals[name].Compile(compiledPtrs, nil)
+		if err != nil {
+			return nil, err
+		}
+	}
+
+	compiledMap := make(map[string]machine.FreeExpr)
+	for i, name := range globalNames {
+		compiledMap[name] = compiled[i]
+	}
+
+	return compiledMap, nil
+}
+
+type CompileError struct {
+	Node Node
+	Msg  string
+}
+
+func (ce *CompileError) Error() string {
+	return ce.Msg
+}
+
+type Node interface {
+	MetaInfo() interface{}
+	HasFree(name string) bool
+	Compile(globals map[string]*machine.FreeExpr, free []string) (machine.FreeExpr, error)
+}
+
+type Var struct {
+	Name string
+	Meta interface{}
+}
+
+func (v *Var) MetaInfo() interface{}    { return v.Meta }
+func (v *Var) HasFree(name string) bool { return name == v.Name }
+
+func (v *Var) Compile(globals map[string]*machine.FreeExpr, free []string) (machine.FreeExpr, error) {
+	if len(free) != 1 || free[0] != v.Name {
+		return nil, &CompileError{
+			Node: v,
+			Msg:  fmt.Sprintf("'%s' not defined", v.Name),
+		}
+	}
+	return &machine.FreeVar{
+		Meta: v.Meta,
+	}, nil
+}
+
+type Abst struct {
+	Bound string
+	Body  Node
+	Meta  interface{}
+}
+
+func (a *Abst) MetaInfo() interface{} { return a.Meta }
+func (a *Abst) HasFree(name string) bool {
+	return name != a.Bound && a.Body.HasFree(name)
+}
+
+func (a *Abst) Compile(globals map[string]*machine.FreeExpr, free []string) (machine.FreeExpr, error) {
+	if !a.Body.HasFree(a.Bound) {
+		body, err := a.Body.Compile(globals, free)
+		if err != nil {
+			return nil, err
+		}
+		return &machine.FreeAbst{
+			Used: false,
+			Body: body,
+			Meta: a.Meta,
+		}, nil
+	}
+
+	freeWithBound := append([]string{a.Bound}, free...)
+	body, err := a.Body.Compile(globals, freeWithBound)
+	if err != nil {
+		return nil, err
+	}
+	return &machine.FreeAbst{
+		Used: true,
+		Body: body,
+		Meta: a.Meta,
+	}, nil
+}
+
+type Appl struct {
+	Left, Right Node
+	Meta        interface{}
+}
+
+func (ap *Appl) MetaInfo() interface{} { return ap.Meta }
+func (ap *Appl) HasFree(name string) bool {
+	return ap.Left.HasFree(name) || ap.Right.HasFree(name)
+}
+
+func (ap *Appl) Compile(globals map[string]*machine.FreeExpr, free []string) (machine.FreeExpr, error) {
+	dirs := make([]machine.Dir, len(free))
+	lfree, rfree := []string(nil), []string(nil)
+	for i, name := range free {
+		if ap.Left.HasFree(name) {
+			dirs[i] |= machine.DirLeft
+			lfree = append(lfree, name)
+		}
+		if ap.Right.HasFree(name) {
+			dirs[i] |= machine.DirRight
+			rfree = append(rfree, name)
+		}
+	}
+
+	left, err := ap.Left.Compile(globals, lfree)
+	if err != nil {
+		return nil, err
+	}
+	right, err := ap.Right.Compile(globals, rfree)
+	if err != nil {
+		return nil, err
+	}
+
+	return &machine.FreeAppl{
+		Dirs:  dirs,
+		Left:  left,
+		Right: right,
+		Meta:  ap.Meta,
+	}, nil
+}
+
+type Global struct {
+	Name string
+	Meta interface{}
+}
+
+func (g *Global) MetaInfo() interface{}    { return g.Meta }
+func (g *Global) HasFree(name string) bool { return false }
+
+func (g *Global) Compile(globals map[string]*machine.FreeExpr, free []string) (machine.FreeExpr, error) {
+	global, ok := globals[g.Name]
+	if !ok {
+		return nil, &CompileError{
+			Node: g,
+			Msg:  fmt.Sprintf("'%s' not defined", g.Name),
+		}
+	}
+	return &machine.FreeRef{
+		Ref: global,
+	}, nil
+}
---
+++ machine/lambda.go
@@ -0,0 +1,161 @@
+package machine
+
+var ApplicationCallback func(left, right Expr)
+
+type Ctx struct {
+	Expr Expr
+	Next *Ctx
+}
+
+type Dir uint8
+
+const (
+	DirLeft Dir = 1 + iota
+	DirRight
+	DirBoth
+)
+
+type Expr interface {
+	MetaInfo() interface{}
+	IsNormal() bool
+	Reduce() Expr
+}
+
+type FreeExpr interface {
+	MetaInfo() interface{}
+	Fill(ctx *Ctx) Expr
+}
+
+type FreeVar struct {
+	Meta interface{}
+}
+
+func (fv *FreeVar) MetaInfo() interface{} { return fv.Meta }
+
+func (fv *FreeVar) Fill(ctx *Ctx) Expr {
+	if ctx == nil {
+		panic("free var: no context values")
+	}
+	if ctx.Next != nil {
+		panic("free var: context has more than one value")
+	}
+	return ctx.Expr
+}
+
+type FreeAbst struct {
+	Used bool
+	Body FreeExpr
+	Meta interface{}
+}
+
+func (fa *FreeAbst) MetaInfo() interface{} { return fa.Meta }
+
+func (fa *FreeAbst) Fill(ctx *Ctx) Expr {
+	return &Abst{
+		Ctx:  ctx,
+		Used: fa.Used,
+		Body: fa.Body,
+		Meta: fa.Meta,
+	}
+}
+
+type FreeAppl struct {
+	Dirs        []Dir
+	Left, Right FreeExpr
+	Meta        interface{}
+}
+
+func (fap *FreeAppl) MetaInfo() interface{} { return fap.Meta }
+
+func (fap *FreeAppl) Fill(ctx *Ctx) Expr {
+	lctx, rctx := distribute(fap.Dirs, ctx)
+	left, right := fap.Left.Fill(lctx), fap.Right.Fill(rctx)
+	return &Appl{
+		Left:  left,
+		Right: right,
+		Meta:  fap.Meta,
+	}
+}
+
+func distribute(dirs []Dir, ctx *Ctx) (left, right *Ctx) {
+	for _, dir := range dirs {
+		if ctx == nil {
+			panic("distribute: context too short")
+		}
+		if dir&DirLeft != 0 {
+			left = cons(ctx.Expr, left)
+		}
+		if dir&DirRight != 0 {
+			right = cons(ctx.Expr, right)
+		}
+		ctx = ctx.Next
+	}
+	if ctx != nil {
+		panic("distribute: context too long")
+	}
+	return left, right
+}
+
+type FreeRef struct {
+	Ref *FreeExpr
+}
+
+func (fr *FreeRef) MetaInfo() interface{} { return (*fr.Ref).MetaInfo() }
+func (fr *FreeRef) Fill(ctx *Ctx) Expr    { return (*fr.Ref).Fill(ctx) }
+
+type Abst struct {
+	Ctx  *Ctx
+	Used bool
+	Body FreeExpr
+	Meta interface{}
+}
+
+func (a *Abst) MetaInfo() interface{} { return a.Meta }
+func (a *Abst) IsNormal() bool        { return true }
+func (a *Abst) Reduce() Expr          { return a }
+
+type Appl struct {
+	Left, Right Expr
+	Memo        Expr
+	Meta        interface{}
+}
+
+func (ap *Appl) MetaInfo() interface{} { return ap.Meta }
+func (ap *Appl) IsNormal() bool        { return false }
+
+func (ap *Appl) Reduce() (reduced Expr) {
+	if ap.Memo != nil {
+		ap.Memo = ap.Memo.Reduce()
+		return ap.Memo
+	}
+	defer func() {
+		ap.Memo = reduced
+		ap.Left = nil
+		ap.Right = nil
+	}()
+	if !ap.Left.IsNormal() {
+		return &Appl{
+			Left:  ap.Left.Reduce(),
+			Right: ap.Right,
+		}
+	}
+	abst, ok := ap.Left.(*Abst)
+	if !ok {
+		panic("reduce appl: left side not abst")
+	}
+	ctx := abst.Ctx
+	if abst.Used {
+		ctx = cons(ap.Right, ctx)
+	}
+	if ApplicationCallback != nil {
+		ApplicationCallback(ap.Left, ap.Right)
+	}
+	return abst.Body.Fill(ctx)
+}
+
+func cons(expr Expr, ctx *Ctx) *Ctx {
+	return &Ctx{
+		Expr: expr,
+		Next: ctx,
+	}
+}
