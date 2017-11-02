31f7cfcf20a0f60bdf6f39eb069bcc56ea3e2053
faiface <faiface@ksp.sk>
2017-11-02 19:42:06 +0100

    add parse package

---
+++ parse/parse.go
@@ -0,0 +1,306 @@
+package parse
+
+import (
+	"fmt"
+	"io"
+	"io/ioutil"
+	"strings"
+	"unicode"
+	"unicode/utf8"
+
+	"github.com/faiface/lambda/ast"
+)
+
+type FileInfo struct {
+	Filename     string
+	Line, Column int
+}
+
+func (fi *FileInfo) Add(filename string, line, column int) *FileInfo {
+	if fi == nil {
+		return &FileInfo{
+			Filename: filename,
+			Line:     line,
+			Column:   column,
+		}
+	}
+	return &FileInfo{
+		Filename: filename,
+		Line:     fi.Line + line - 1,
+		Column:   fi.Column + column - 1,
+	}
+}
+
+type Error struct {
+	*FileInfo
+	Msg string
+}
+
+func (err *Error) Add(filename string, line, column int) *Error {
+	return &Error{
+		FileInfo: err.FileInfo.Add(filename, line, column),
+		Msg:      err.Msg,
+	}
+}
+
+func (err *Error) Error() string {
+	if err.FileInfo == nil {
+		return err.Msg
+	}
+	return fmt.Sprintf("%s:%d:%d: %s", err.Filename, err.Line, err.Column, err.Msg)
+}
+
+func Single(r io.Reader) (ast.Node, error) {
+	data, err := ioutil.ReadAll(r)
+	if err != nil {
+		return nil, err
+	}
+	toks := Tokenize(data)
+	if err != nil {
+		return nil, err
+	}
+	return SingleFromTokens(toks)
+}
+
+func Definitions(r io.Reader) (map[string]ast.Node, error) {
+	data, err := ioutil.ReadAll(r)
+	if err != nil {
+		return nil, err
+	}
+	toks := Tokenize(data)
+	if err != nil {
+		return nil, err
+	}
+	return DefinitionsFromTokens(toks)
+}
+
+func SingleFromTokens(toks []Token) (ast.Node, error) {
+	var node ast.Node
+
+	for i := 0; i < len(toks); i++ {
+		tok := toks[i]
+		var right ast.Node
+
+		switch tok.Token {
+		case "(":
+			match := matchingParen(toks[i+1:])
+			if match == -1 {
+				return nil, &Error{
+					FileInfo: tok.FileInfo,
+					Msg:      "no matching right parenthesis",
+				}
+			}
+			var err error
+			right, err = SingleFromTokens(toks[i+1 : i+1+match])
+			if err != nil {
+				return nil, err
+			}
+			if right == nil {
+				return nil, &Error{
+					FileInfo: tok.FileInfo,
+					Msg:      "no expression inside parenthesis",
+				}
+			}
+			i += 1 + match
+		case ")":
+			return nil, &Error{
+				FileInfo: tok.FileInfo,
+				Msg:      "no matching left parenthesis",
+			}
+		case "\\", "λ":
+			if len(toks[i+1:]) == 0 {
+				return nil, &Error{
+					FileInfo: tok.FileInfo,
+					Msg:      fmt.Sprintf("no binding after '%s'", tok.Token),
+				}
+			}
+			bound := toks[i+1].Token
+			if bound == strings.Title(bound) {
+				return nil, &Error{
+					FileInfo: tok.FileInfo,
+					Msg:      fmt.Sprintf("invalid bound name '%s'", bound),
+				}
+			}
+			switch bound {
+			case "(", ")", "\\", "λ", ";":
+				return nil, &Error{
+					FileInfo: tok.FileInfo,
+					Msg:      fmt.Sprintf("invalid bound name '%s'", bound),
+				}
+			}
+			body, err := SingleFromTokens(toks[i+2:])
+			if err != nil {
+				return nil, err
+			}
+			return wrapAppl(node, &ast.Abst{
+				Bound: toks[i+1].Token,
+				Body:  body,
+				Meta:  tok.FileInfo,
+			}), nil
+		case ";":
+			var err error
+			right, err = SingleFromTokens(toks[i+1:])
+			if err != nil {
+				return nil, err
+			}
+		default:
+			identifier := tok.Token
+			if identifier == strings.Title(identifier) {
+				right = &ast.Global{
+					Name: identifier,
+					Meta: tok.FileInfo,
+				}
+			} else {
+				right = &ast.Var{
+					Name: identifier,
+					Meta: tok.FileInfo,
+				}
+			}
+		}
+
+		node = wrapAppl(node, right)
+	}
+
+	return node, nil
+}
+
+func matchingParen(toks []Token) int {
+	var (
+		i    int
+		tok  Token
+		nest = 1
+	)
+	for i, tok = range toks {
+		switch tok.Token {
+		case "(":
+			nest++
+		case ")":
+			nest--
+		}
+		if nest == 0 {
+			break
+		}
+	}
+	if nest != 0 {
+		return -1
+	}
+	return i
+}
+
+func wrapAppl(left, right ast.Node) ast.Node {
+	if left == nil {
+		return right
+	}
+	return &ast.Appl{
+		Left:  left,
+		Right: right,
+		Meta:  nil,
+	}
+}
+
+func DefinitionsFromTokens(toks []Token) (map[string]ast.Node, error) {
+	defs := make(map[string]ast.Node)
+	for len(toks) > 0 {
+		name, node, ends, err := definition(toks)
+		if err != nil {
+			return nil, err
+		}
+		defs[name] = node
+		toks = toks[ends:]
+	}
+	return defs, nil
+}
+
+func definition(toks []Token) (name string, node ast.Node, ends int, err error) {
+	if len(toks) < 3 || toks[1].Token != "=" {
+		return "", nil, 0, &Error{
+			Msg: "no or invalid definition",
+		}
+	}
+	name = toks[0].Token
+	if name != strings.Title(name) || name == "=" {
+		return "", nil, 0, &Error{
+			FileInfo: toks[0].FileInfo,
+			Msg:      fmt.Sprintf("invalid global name '%s'", name),
+		}
+	}
+	for i := 2; i < len(toks); i++ {
+		if toks[i].Token == "=" {
+			ends = i - 1
+			break
+		}
+	}
+	if ends == 0 {
+		ends = len(toks)
+	}
+	node, err = SingleFromTokens(toks[2:ends])
+	if err != nil {
+		return "", nil, 0, err
+	}
+	return name, node, ends, nil
+}
+
+func Tokenize(data []byte) []Token {
+	var (
+		tokens []Token
+		token  []rune
+		i      = 0
+		line   = 1
+		column = 1
+	)
+
+	flushToken := func(line, column int) {
+		if len(token) == 0 {
+			return
+		}
+		tokens = append(tokens, Token{
+			FileInfo: &FileInfo{
+				Line:   line,
+				Column: column,
+			},
+			Token: string(token),
+		})
+		token = token[:0]
+	}
+
+nextToken:
+	for i < len(data) {
+		firstColumn := column
+		for i < len(data) {
+			r, width := utf8.DecodeRune(data[i:])
+			i += width
+			column++
+			if r == '\n' {
+				line++
+				column = 1
+			}
+			if unicode.IsSpace(r) {
+				break
+			}
+			switch r {
+			case '(', ')', '\\', 'λ', ';':
+				flushToken(line, firstColumn)
+				token = append(token[:0], r)
+				flushToken(line, column-1)
+				continue nextToken
+			default:
+				token = append(token, r)
+			}
+		}
+		flushToken(line, firstColumn)
+	}
+
+	return tokens
+}
+
+type Token struct {
+	*FileInfo
+	Token string
+}
+
+func (tok Token) Add(filename string, line, column int) Token {
+	return Token{
+		FileInfo: tok.FileInfo.Add(filename, line, column),
+		Token:    tok.Token,
+	}
+}
